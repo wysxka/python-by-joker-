e1b9875c52a0cfdf472761a03d2447d4372faee6
wysxka <51552431+wysxka@users.noreply.github.com>
2019-09-10 08:32:27 +0800

    Update day 01.py

--- day 01.py
+++ day 01.py
@@ -79,23 +79,4 @@ if (year % 4 == 0 and year % 100 != 0 or year% 400==0):
 else:
     print('%d 不是闰年'%year)
 
-#输入一个月份判断有多少天
-a = eval(input('请输入月份：'))
- 
-while not(isinstance(a, int) and 0<a<13):
-    a = eval(input('请输入正确的月份：'))
- 
-da = [1,3,5,7,10,12]
-xiao = [4,6,8,9,11]
- 
-if (a in da):
-    print(a,'月有31天')
-elif (a in xiao):
-    print(a,'月有30天')
-else:
-    n=eval(input('请输入月所在年：'))
-    if  n%400==0 or (n%4==0 and n%100!=0):
-        print(n,'年为闰年',a,'月有29天')
-    else:
-        print(n,'年为平年',a,'月有28天')
 
